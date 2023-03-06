bec85efae30eab0613021458319541919775f157
Byron OjuaNice <niceb@os1.engr.oregonstate.edu>
2023-03-05 17:56:08 -0800

    Added template code to enc files

---
+++ enc_client.h
@@ -0,0 +1,12 @@
+#ifndef ENC_CLIENT_H
+#define ENC_CLIENT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>  // ssize_t
+#include <sys/socket.h> // send(),recv()
+#include <netdb.h>      // gethostbyname()
+
+#endif // !ENC_CLIENT_H
---
+++ enc_server.h
@@ -0,0 +1,12 @@
+#ifndef ENC_CLIENT_H
+#define ENC_CLIENT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#endif // !ENC_CLIENT_H
